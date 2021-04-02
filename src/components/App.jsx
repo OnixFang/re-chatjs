323a0e382a46b79c96023525e0d666ed5f59fd95
OnixFang <twily.dev@outlook.com>
2021-04-02 15:19:53 -0400

    Cleanup and setup App component with login and chat

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,24 @@
-export default function App() {
-  return (
-    <div>
-      <h1>Re-Chat.js</h1>
-    </div>
-  );
+import React from 'react';
+import { io } from 'socket.io-client';
+import Login from './Login';
+import Chat from './Chat';
+const endpoint = 'http://rechatjsapi:9010';
+let socket;
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: '',
+      user: null,
+    };
+  }
+
+  login = (username) => {
+    this.setState({ user: { username } });
+  };
+
+  render() {
+    return this.state.user ? <Chat /> : <Login login={this.login} />;
+  }
 }
