48211fb39982f4742bc7822390a5fd91e052f23f
OnixFang <twily.dev@outlook.com>
2021-04-02 15:19:42 -0400

    Create Login component

---
+++ src/components/Login.jsx
@@ -0,0 +1,23 @@
+import { useRef } from 'react';
+
+export default function Login(props) {
+  const username = useRef();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    props.login(username.current.value);
+  };
+
+  return (
+    <div className="valign-wrapper">
+      <div className="login">
+        <h1 className="headline">Login</h1>
+        <form className="login-form" onSubmit={handleSubmit}>
+          <label htmlFor="username">Username</label>
+          <input className="text-input" id="username" type="text" ref={username} required />
+          <input className="btn" type="submit" value="Login" />
+        </form>
+      </div>
+    </div>
+  );
+}
