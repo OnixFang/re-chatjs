3850bf8f1e9a59eba4b5340fbd1a0ba3838a6a06
OnixFang <twily.dev@outlook.com>
2021-04-02 18:23:33 -0400

    Setup initial Chat UI

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import { io } from 'socket.io-client';
 const endpoint = 'http://rechatjsapi:9010';
 let socket;
@@ -17,9 +18,51 @@ export default function Chat(props) {
   //   socket.disconnect();
   // }
 
+  const message = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(message.current.value);
+    message.current.value = '';
+  };
+
   return (
-    <div>
-      <h1>Hello!</h1>
+    <div className="chat">
+      <div className="chat-rooms">
+        <div className="chat-room active">
+          <img className="room-image" src="./assets/group.svg" alt="Room image" />
+          <div className="room-info">
+            <span className="room-name">Live chat</span>
+          </div>
+        </div>
+        <div className="chat-room">
+          <img className="room-image" src="./assets/group.svg" alt="Room image" />
+          <div className="room-info">
+            <span className="room-name">Live chat 2</span>
+          </div>
+        </div>
+      </div>
+      <div className="active-chat">
+        <div className="room-bar">
+          <img className="room-image" src="./assets/group.svg" alt="Room image" />
+          <span className="room-name">Live chat</span>
+        </div>
+        <div className="chat-window"></div>
+        <form className="chat-input" onSubmit={handleSubmit} autoComplete="off">
+          <input
+            className="text-input"
+            type="text"
+            id="message"
+            ref={message}
+            required
+            placeholder="Write a message..."
+          />
+          <button className="btn icon-btn" type="submit">
+            <span className="icon send"></span>
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
