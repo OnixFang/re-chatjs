84c32d3c7b33ff29ac57bdea7e26ceb343697124
OnixFang <twily.dev@outlook.com>
2021-04-02 15:19:35 -0400

    Create Chat component

---
+++ src/components/Chat.jsx
@@ -0,0 +1,21 @@
+export default function Chat(props) {
+  // componentDidMount() {
+  //   try {
+  //     socket = io(endpoint, { transports: ['websocket'] });
+
+  //     socket.emit('set user', this.state.user);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+  // componentWillUnmount() {
+  //   socket.disconnect();
+  // }
+
+  return (
+    <div>
+      <h1>Hello!</h1>
+    </div>
+  );
+}
