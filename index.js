0f53c4d625c676d1c4d09df386c6f25df7add6a8
OnixFang <twily.dev@outlook.com>
2021-04-04 12:37:03 -0400

    Setup error handler

--- index.js
+++ index.js
@@ -21,6 +21,18 @@ app.use((req, res, next) => {
 // API
 app.use(apiRouter);
 
+// 404 error handler
+app.use((req, res, next) => {
+  const error = new Error('API route not found');
+  error.status = 404;
+  next(error);
+});
+
+// Error handler
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).send(error.message);
+});
+
 const server = app.listen(PORT, () => {
   console.log(`App running on http://${HOST}:${PORT}`);
 });
