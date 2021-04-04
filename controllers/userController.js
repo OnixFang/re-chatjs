b7187f2bdb6ebee9eaadc7d134f235dabdbe8c17
OnixFang <twily.dev@outlook.com>
2021-04-04 10:24:00 -0400

    Create async user controller

---
+++ controllers/userController.js
@@ -0,0 +1,50 @@
+const fs = require('fs').promises;
+const filename = 'mockdata.json';
+const HttpError = require('../customErrors/HttpError');
+
+const getDatabase = async () => {
+  try {
+    return JSON.parse(await fs.readFile(filename));
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getUser = async (username) => {
+  try {
+    const database = await getDatabase();
+    const user = database.users.find((user) => user.username.toLowerCase() === username.toLowerCase());
+    if (user) {
+      return user;
+    } else {
+      throw new HttpError('User not found', 404);
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+const addUser = async (username) => {
+  try {
+    const database = await getDatabase();
+
+    if (!database.users.find((user) => user.username.toLowerCase() === username.toLowerCase())) {
+      const newUser = {
+        username,
+      };
+      database.users.push(newUser);
+      await fs.writeFile(filename, database);
+
+      return newUser;
+    } else {
+      throw new HttpError('Username already exists', 409);
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = {
+  getUser,
+  addUser,
+};
