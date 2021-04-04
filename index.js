eef5561676af8c4295be201a0afb3f7b7537c474
OnixFang <twily.dev@outlook.com>
2021-04-04 10:24:48 -0400

    Setup controllers on server index

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+// Modules
 const express = require('express');
 const socketIo = require('socket.io');
 const app = express();
-let testvar = 0;
 
-const socketHandler = require('./controllers/socketController');
+// Controllers
+const { login } = require('./controllers/authController');
+const socketController = require('./controllers/socketController');
+const apiRouter = require('./apiRouter');
 
 const { HOST, PORT } = process.env;
 
@@ -11,13 +14,12 @@ app.use(express.json());
 
 // CORS compliant request middleware
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', `http://${HOST}:*`);
   next();
 });
 
-app.get('/', (req, res, next) => {
-  res.status(200).send('Server running OK');
-});
+// API
+app.use(apiRouter);
 
 const server = app.listen(PORT, () => {
   console.log(`App running on http://${HOST}:${PORT}`);
@@ -25,4 +27,4 @@ const server = app.listen(PORT, () => {
 
 const io = socketIo(server);
 
-io.on('connection', socketHandler);
+io.on('connection', socketController);
