12e5f6dd7bae59a141a8e807049ffdddf562acb5
OnixFang <twily.dev@outlook.com>
2021-04-05 00:56:29 -0400

    Comment code and refactor disconnect notification

--- controllers/socketController.js
+++ controllers/socketController.js
@@ -8,18 +8,25 @@ const socketController = (io) => {
     });
   };
 
+  // Returns an array of current online users
   const usersInRoom = () => {
     const userArray = [];
-    io.sockets.adapter.rooms.get(liveChat).forEach((scket) => {
-      userArray.push(io.sockets.sockets.get(scket).user.username);
-    });
-    return userArray;
+    const chatRoom = io.sockets.adapter.rooms.get(liveChat);
+    if (chatRoom) {
+      chatRoom.forEach((scket) => {
+        userArray.push(io.sockets.sockets.get(scket).user.username);
+      });
+      return userArray;
+    } else {
+      return [];
+    }
   };
 
   io.on('connection', (socket) => {
     console.log('A user connected!');
     socket.join(liveChat);
 
+    // Get and attach user object to socket
     socket.on('set user', (payload) => {
       const { user, rooms } = payload;
       socket.user = user;
@@ -33,12 +40,15 @@ const socketController = (io) => {
         dateSent: Date.now(),
       };
 
+      // Add user to current users in chat
       const currentUsers = usersInRoom().sort();
-
       io.to(liveChat).emit('current users', currentUsers);
+
+      // Send message of new user joining chat
       io.to(liveChat).emit('chat message', message);
     });
 
+    // Chat message handler
     socket.on('chat message', (msg) => {
       io.to(msg.room).emit('chat message', msg);
     });
@@ -52,7 +62,15 @@ const socketController = (io) => {
           message: `${username} left the chat.`,
           dateSent: Date.now(),
         };
+
+        // Remove user from current users in chat
         mockLogout(username);
+        const currentUsers = usersInRoom().sort();
+        if (currentUsers.length) {
+          io.to(liveChat).emit('current users', currentUsers);
+        }
+
+        // Send message of user leaving chat
         io.to(liveChat).emit('chat message', message);
         console.log('User disconnected: ', username);
       }
